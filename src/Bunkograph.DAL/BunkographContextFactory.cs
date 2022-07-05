ecc5b89b7fcdf98fb88cfef2b7f77e8db8df2e08
Andrew Timson <6879630+HamsterExAstris@users.noreply.github.com>
2022-07-04 21:18:01 -0400

    Fix DbContextOptions instantiation at design-time for scaffolding.
    Migrations work out-of-the-box with the .NET 6.0 patterns, but not scaffolding.

---
+++ src/Bunkograph.DAL/BunkographContextFactory.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Reflection;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Bunkograph.DAL
+{
+    public class BunkographContextFactory : IDesignTimeDbContextFactory<BunkographContext>
+    {
+        public BunkographContext CreateDbContext(string[] args)
+        {
+            // Create a configuration using the user secrets from the web project, where we expect
+            // developers to store their connection string.
+            Assembly? assembly = Assembly.Load("Bunkograph.Web");
+            IConfigurationBuilder? configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddUserSecrets(assembly);
+            IConfigurationRoot? configuration = configurationBuilder.Build();
+
+            string? connectionString = configuration.GetConnectionString("localdb")
+                ?? throw new InvalidOperationException("Database connection string 'localdb' not found.");
+
+            ServerVersion? serverVersion = ServerVersion.AutoDetect(connectionString);
+
+            DbContextOptionsBuilder<BunkographContext>? optionsBuilder = new DbContextOptionsBuilder<BunkographContext>();
+            optionsBuilder.UseMySql(connectionString, serverVersion);
+
+            return new BunkographContext(optionsBuilder.Options);
+        }
+    }
+}
