df3a00424a74ae86334aea29811669dbf3f14760
Andrew Timson <6879630+HamsterExAstris@users.noreply.github.com>
2022-07-09 11:29:38 -0400

    Fix MSAL outside the dev environment. (#10)
    Auth tweaks.
    * Let MSAL determine the redirect URI.
    * Use local storage instead of session storage so the token persists.

--- src/Bunkograph.Web/ClientApp/src/authConfig.ts
+++ src/Bunkograph.Web/ClientApp/src/authConfig.ts
@@ -2,10 +2,10 @@ export const msalConfig = {
   auth: {
     clientId: "6ea66498-a20d-46e5-822c-a767f059fb02",
     authority: "https://login.microsoftonline.com/abb1108c-49cc-4f42-85d5-50dec480ae44", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
-    redirectUri: "https://localhost:44405",
+    // The sample in the docs shows redirectUri being specified, but the app should figure it out by default.
   },
   cache: {
-    cacheLocation: "sessionStorage", // This configures where your cache will be stored
+    cacheLocation: "localStorage", // This configures where your cache will be stored
     storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
   }
 };
