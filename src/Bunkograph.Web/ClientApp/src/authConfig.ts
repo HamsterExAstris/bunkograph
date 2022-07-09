cbd6e001e41f9a133fb1d737de3450c651c82a30
Andrew Timson <6879630+HamsterExAstris@users.noreply.github.com>
2022-07-09 11:53:49 -0400

    Fix login from subpages.

--- src/Bunkograph.Web/ClientApp/src/authConfig.ts
+++ src/Bunkograph.Web/ClientApp/src/authConfig.ts
@@ -2,7 +2,8 @@ export const msalConfig = {
   auth: {
     clientId: "6ea66498-a20d-46e5-822c-a767f059fb02",
     authority: "https://login.microsoftonline.com/abb1108c-49cc-4f42-85d5-50dec480ae44", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
-    // The sample in the docs shows redirectUri being specified, but the app should figure it out by default.
+    // redirectUri MUST be specified, as it must match exactly what's in the app registration. If it isn't, MSAL uses the current URL, and subpages don't match.
+    redirectUri: window.location.origin
   },
   cache: {
     cacheLocation: "localStorage", // This configures where your cache will be stored
